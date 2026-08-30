8befd7f9e6e19fb71635d4b74eeb6678e13f8e5f
ndgt310 <rinnoir@icloud.com>
2026-08-30 17:59:47 +0900

    掲示板をテンプレ化、誤字、リンク切れを修正

--- bio.php
+++ bio.php
@@ -3,7 +3,43 @@ require_once 'header.php';
 ?>
 <h1>バイオグラフィー</h1>
     工事中です
+<br><br>
+バイオグラフィーに記載してほしい事項を募集します。<br>
+<br>
+  $name = htmlspecialchars($_POST['name']);
+下の画面に表示しても良い場合はチェック<br>
 
+<form method="post" action="">
+  	<input type="checkbox" name="visible[]" value="表示"> 表示してもよい
 
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+</form>
+<
+
+    <br><br>
+    ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+    <br>
+    <p>2003/02/23 誕生</p>
+    <br>
+    <p>2006/04/11 丸亀聖母幼稚園 入園</p>
+    <br>
+    <p>2009/04/07 丸亀市立城坤小学校　入学</p>
+    <br>
+    <p>2015/04/07 丸亀市立西中学校　入学</p>
+    <br>
+    <p>2018/04/09 香川県立坂出高等学校 普通科　入学</p>
+    <br>
+    2021/04/01 関西学院大学 社会学部　入学
+    <br>
+    <p>2025/04/01 就職</p>
+<br><br>
+☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+<br><br>
+    投稿のうち、検討中の内容はここから下に表示されます
+    <br>
+    ---------------
+<br><br><br><br>
+
+    
+    ---------------
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       © 2026 Kameyama Tamao All Rights Reserved.
